a176db8d53c6d97e68e6e960e25e80c47f9b0db0
renato3x <pereirarenato21@gmail.com>
2024-09-25 22:22:42 -0300

    add: register user mutation

--- resolvers/mutation/user.js
+++ resolvers/mutation/user.js
@@ -1,14 +1,34 @@
+const bcrypt = require('bcrypt');
 const knex = require('../../config/db');
 
 module.exports = {
+  async registerUser(_, { data }) {
+    const { profileId } = await knex('profiles')
+      .select('id as profileId')
+      .where({ name: 'common' })
+      .first();
+    
+    return await knex('users').insert({
+      ...data,
+      password: bcrypt.hashSync(data.password, 10)
+    }).then(async ([ userId ]) => {
+      await knex('users_profiles').insert({ userId, profileId });
+
+      return await knex('users').select().where({ id: userId }).first();
+    });
+  },
   async newUser(_, { data }) {
     const dataClone = JSON.parse(JSON.stringify(data))
     const profiles = [];
 
-    if (dataClone.profiles) {
+    if (!dataClone.profiles || dataClone.profiles.length === 0) {
+      profiles.push({ name: 'common' });
+    } else {
       profiles.push(...data.profiles);
       delete dataClone.profiles;
-    }
+    } 
+
+    dataClone.password = bcrypt.hashSync(dataClone.password, 10);
 
     return await knex('users')
       .insert(dataClone)
@@ -47,27 +67,37 @@ module.exports = {
     const user = await knex('users').select().where(filters).first();
 
     if (user) {
-      const cloneData = JSON.parse(JSON.stringify(data));
+      const dataClone = JSON.parse(JSON.stringify(data));
       const profiles = [];
 
-      if (cloneData.profiles) {
+      if (dataClone.profiles) {
         profiles.push(...data.profiles);
-        delete cloneData.profiles;
+        delete dataClone.profiles;
+      }
+
+      if (dataClone.password) {
+        dataClone.password = bcrypt.hashSync(dataClone.password, 10);
       }
 
       return await knex('users')
-        .update(cloneData)
+        .update(dataClone)
         .where('id', '=', user.id)
         .then(async () => {
-          for (const profile of profiles) {
-            const profileExists = await knex
-              .select('id')
-              .from('profiles')
-              .where(profile)
-              .first();
-    
-            if (profileExists) {
-              await knex('users_profiles').insert({ userId: user.id, profileId: profileExists.id });
+          if (profiles.length > 0) {
+            await knex('users_profiles')
+              .delete()
+              .where('userId', '=', user.id);
+  
+            for (const profile of profiles) {
+              const profileExists = await knex
+                .select('id')
+                .from('profiles')
+                .where(profile)
+                .first();
+      
+              if (profileExists) {
+                await knex('users_profiles').insert({ userId: user.id, profileId: profileExists.id });
+              }
             }
           }
 

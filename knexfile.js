e548ec953636e40f1058d5bd6e51d5e7b8f347ce
renato3x <pereirarenato21@gmail.com>
2024-09-29 11:52:56 -0300

    add: knexfile types

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 module.exports = {
   client: 'mysql2',
   connection: {
